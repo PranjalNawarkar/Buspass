6fb9d6ddb8051ee9a710e2446b51d9bb3c2f0828
Pranjal Nawarkar <nawarkarpranjal@gmail.com>
2021-08-31 11:35:32 +0530

    User reregisteration

---
+++ app/src/main/java/com/example/prady/BusPass/ReRegistration.java
@@ -0,0 +1,275 @@
+package com.example.prady.BusPass;
+
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ReRegistration extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    DatabaseHelper db;
+    String inputValue="";
+    Button bRegister,btnSearch;
+    public static EditText etName, etDOB, etPass, etPass1,etCity, etUser, etPhone, etAdharNo,etPhoneSearch;
+    TextView tvUploadDocLink;
+    Spinner spinner;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+    DatePickerDialog picker;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_re_registration);
+
+        db = new DatabaseHelper(this);
+        btnSearch=findViewById(R.id.btnSearch);
+        etPhoneSearch=findViewById(R.id.etPhoneSearch);
+        etName = (EditText) findViewById(R.id.etName);
+        etDOB = (EditText) findViewById(R.id.etDOB);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        etUser = (EditText) findViewById(R.id.etUser);
+        etPass = (EditText) findViewById(R.id.etPass);
+        etCity = (EditText) findViewById(R.id.etCity);
+        etPass1 = (EditText) findViewById(R.id.etPass1);
+        etAdharNo = (EditText) findViewById(R.id.etAdharNo);
+        bRegister = (Button) findViewById(R.id.bRegister);
+        spinner = findViewById(R.id.spAgeGroup);
+
+       // tvUploadDocLink = (TextView) findViewById(R.id.tvUploadDocLink);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String a = etPhoneSearch.getText().toString();
+
+                SQLiteDatabase db1=db.getReadableDatabase();
+                Cursor cursor=db1.rawQuery("select * from registerShop where Phone=?", new String[]{a});
+
+                cursor.moveToFirst();
+                int cnt= cursor.getCount();
+                if(cnt > 0) {
+                    String name = cursor.getString(cursor.getColumnIndex("Name"));
+                    String phone = cursor.getString(cursor.getColumnIndex("Phone"));
+                    String adharno = cursor.getString(cursor.getColumnIndex("AdharNo"));
+                    String city = cursor.getString(cursor.getColumnIndex("City"));
+                    String dob = cursor.getString(cursor.getColumnIndex("DOB"));
+
+                String output = "Name=" + name + "\nPhone=" + phone + "\nAadhar No=" + adharno;
+                  //  inputValue = output;
+
+
+                    etName.setText(name);
+                    etPhone.setText(phone);
+                    etAdharNo.setText(adharno);
+                    etCity.setText(city);
+                    etDOB.setText(dob);
+
+
+
+
+
+
+                   //  Toast.makeText(ReRegistration.this, "text: " + output, Toast.LENGTH_SHORT).show();
+
+
+                    SharedPreferences sp2 = getSharedPreferences("key2", Context.MODE_PRIVATE);
+                    String user = sp2.getString("usern", "");
+
+                }
+
+
+            }
+        });
+
+
+        etDOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(ReRegistration.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                etDOB.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+
+        //  Date d = new Date();
+        // CharSequence cdate  = DateFormat.format("MMMM d, yyyy ", d.getTime());
+        //String s=String.valueOf(cdate);
+
+
+        //  Toast.makeText(Registration.this, "Fields cant be empty"+s, Toast.LENGTH_LONG).show();
+
+
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date d = new Date();
+                CharSequence cdate  = DateFormat.format("MMMM d, yyyy ", d.getTime());
+                String s=String.valueOf(cdate);
+
+
+
+
+
+
+                //  Toast.makeText(Registration.this, "Fields cant be empty"+s, Toast.LENGTH_LONG).show();
+                // db=openHelper.getWritableDatabase();
+                String name = etName.getText().toString();
+                String dob= String.valueOf(etDOB.getText());
+
+                String phone = etPhone.getText().toString();
+                String adhar = etAdharNo.getText().toString();
+                String city = etCity.getText().toString();
+                String user = etUser.getText().toString();
+                String pass = etPass.getText().toString();
+                String pass1 = etPass1.getText().toString();
+
+                SharedPreferences sp = getSharedPreferences("key", Context.MODE_PRIVATE);
+                String value = sp.getString("item", "");
+
+
+                if (name.equals("") || name == null || etDOB.getText().equals("") || etDOB.getText() == null || phone.equals("") || phone == null || adhar.equals("") || adhar == null
+                        ||city.equals("") || city == null|| user.equals("") || user == null || pass.equals("") || pass == null || pass1.equals("") || pass1 == null) {
+
+                    Toast.makeText(ReRegistration.this, "Fields cant be empty", Toast.LENGTH_LONG).show();
+                } else {
+                    if(phone.length()==10){
+                        if(adhar.length()==12){
+                            if (pass.equals(pass1)) {
+                                if (db.insertData(name, dob, phone, adhar,city, user, pass, value)) {
+
+                                    try {
+
+                                        java.util.Random r=new java.util.Random();
+
+                                        int otp = r.nextInt(1000000);
+                                        String otp_no=String.valueOf(otp);
+                                        SharedPreferences sp1 = getSharedPreferences("key1", Context.MODE_PRIVATE);
+                                        SharedPreferences.Editor ed1 = sp1.edit();
+                                        ed1.putString("otp_no", otp_no);
+                                        ed1.commit();
+
+                                        String otp_msg="Here is the OTP: " +otp;
+                                        SmsManager smsManager = SmsManager.getDefault();
+                                        smsManager.sendTextMessage(phone, null, otp_msg, null, null);
+                                        Toast.makeText(getApplicationContext(), "OTP Sent!",Toast.LENGTH_LONG).show();
+                                        Intent i = new Intent(ReRegistration.this, Payment1.class);
+                                        startActivity(i);
+
+                                    } catch (Exception e) {
+                                        Toast.makeText(getApplicationContext(),
+                                                "OTP Sending faild, please try again later!",
+                                                Toast.LENGTH_LONG).show();
+                                        e.printStackTrace();
+                                    }
+
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Could not register", Toast.LENGTH_LONG).show();
+                                }
+                            } else {
+                                Toast.makeText(ReRegistration.this, "Password don't match!", Toast.LENGTH_LONG).show();
+
+                            }
+                        }else{
+                            Toast.makeText(ReRegistration.this, "Enter valid Adhar Number!", Toast.LENGTH_LONG).show();
+
+                        }
+                    }else{
+                        Toast.makeText(ReRegistration.this, "Enter valid Phone number!", Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+            }
+
+        });
+
+
+/*
+//        tvUploadDocLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ReRegistration.this, UploadDocument.class));
+            }
+        });
+*/
+
+        spinner.setOnItemSelectedListener(this);//spinner.setOnItemSelectedListener(this);
+        List<String> list = new ArrayList<String>();
+        list.add("Select");
+        list.add("Student");
+        list.add("Adult");
+        list.add("Senior Citizen");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long id) {
+
+        String item = adapterView.getItemAtPosition(i).toString();
+        SharedPreferences sp = getSharedPreferences("key", Context.MODE_PRIVATE);
+        SharedPreferences.Editor ed = sp.edit();
+        ed.putString("item", item);
+        ed.commit();
+
+        //  Intent z = new Intent(this, Payment.class);
+        //  z.putExtra("item_name", item);
+        //  startActivity(z);
+       /* if(i>0){
+           // Toast.makeText(MainActivity.this,"Hello "+item,Toast.LENGTH_SHORT).show();
+        }*/
+    }
+
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
+
+}
